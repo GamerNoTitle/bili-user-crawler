ac4d96ec5e469e8a03209ab46deb5d18b563efe0
Haikido <GamerPesy@outlook.com>
2020-08-01 19:04:51 +0800

    mysql加入执行命令

--- mysqlsave.py
+++ mysqlsave.py
@@ -2,3 +2,8 @@ import pymysql as sql
 def init(address,database,username,password):
     connection=sql.connect(address,user=username,passwd=password,db=database)
     return connection
+
+def execute(connection,command):
+    cursor=connection.cursor()
+    cursor.execute(command)
+    cursor.close()
